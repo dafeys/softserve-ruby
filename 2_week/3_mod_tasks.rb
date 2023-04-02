e07cc0cc81914f9a4ceab9ffa8dc62a69bf8f328
dafeys <dafeys@outlook.com>
2023-04-02 22:34:54 +0300

    3 mod tasks

---
+++ 2_week/3_mod_tasks.rb
@@ -0,0 +1,35 @@
+# # task 1
+# a = 13n
+# b = 11
+# puts (b%a)
+
+# # task 2
+# r = 2
+# PI = 3.1416
+# puts (2 * PI * r)
+
+# # task 3
+# first_person_name = "Petro"
+# second_person_name = "Andrii"
+# puts ( first_person_name > second_person_name ? first_person_name : second_person_name)
+
+# # task 4
+# a = 5
+# b = 10
+
+# def task_4(a, b)
+#     result = (a == b ? 0 : (a < b ? -1 : 1))
+#     puts (result)
+# end
+
+# task_4(a , b)
+
+# task 5
+a = 2.5
+b = 3
+str = "Результат:"
+
+result = ("#{str}\n#{a.to_s}\n#{b.to_s}")
+
+puts (result)
+
