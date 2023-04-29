78bd1e4c0669f3a4c02f481bb19506466ccb98ba
dafeys <dafeys@outlook.com>
2023-04-29 23:23:19 +0300

    update C3DFigure

--- interview_tasks/task3/Classes/C3DFigure.rb
+++ interview_tasks/task3/Classes/C3DFigure.rb
@@ -1,6 +1,6 @@
 # class of a volumetric figure: cylinder or cuboid
 class C3DFigure
-attr_reader :volume
+  attr_reader :volume
 
   def initialize(type, base, figure_height)
     @volume = type.new(base).square * figure_height
